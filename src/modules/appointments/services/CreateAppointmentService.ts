8772e04f3cc5aa0e3416d2d1a4496bf951a94eb2
Leandro Camargo <leandro.bahia@cbyk.com.br>
2021-09-06 18:52:30 -0300

    Finished appointment from user

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -7,6 +7,7 @@ import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
@@ -17,11 +18,29 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepositry,
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
-    const appoitmentDate = startOfHour(date);
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date.");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appontments between 8am and 5pm.',
+      );
+    }
 
     const findAppointmentInSameDate =
-      await this.appointmentsRepository.findByDate(appoitmentDate);
+      await this.appointmentsRepository.findByDate(appointmentDate);
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked');
@@ -29,7 +48,8 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
-      date: appoitmentDate,
+      user_id,
+      date: appointmentDate,
     });
 
     return appointment;
